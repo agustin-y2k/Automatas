7ca3feb12fb6e9c2727e199c1a92ba4db3df7789
Pablo Fracaro <70729831+Pablex05@users.noreply.github.com>
2022-05-02 15:58:50 -0300

    Create url.py

---
+++ practice_work/url.py
@@ -0,0 +1,14 @@
+import re
+
+from pyparsing import Regex, line
+
+Regex = "((http|https|HTTP|HTTPS)?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,3})(\/)?"
+f = open("./texto.txt", "r")
+
+with open("./texto.txt") as archivo:
+    for linea in archivo:
+        if re.fullmatch(Regex,linea.strip()):
+            print(f"url: {linea.strip()}\n \t Expresion Correcta \t")
+        else:
+            print(f"url: {linea.strip()}\n \t Expresion Incorrecta \t")
+        print("-------------------------------------------")
