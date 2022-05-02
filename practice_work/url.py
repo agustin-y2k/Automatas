dfad21bafa266101f2f3879cd5ff6d9c29f76ba5
Pablo Fracaro <70729831+Pablex05@users.noreply.github.com>
2022-05-02 16:08:26 -0300

    Update url.py

--- practice_work/url.py
+++ practice_work/url.py
@@ -3,9 +3,8 @@ import re
 from pyparsing import Regex
 
 Regex = "((http|https|HTTP|HTTPS)?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,3})(\/)?"
-f = open("./url.txt", "r")
 
-with open("./texto.txt") as archivo:
+with open("./url.txt") as archivo:
     for linea in archivo:
         if re.fullmatch(Regex,linea.strip()):
             print(f"url: {linea.strip()}\n \t Expresion Correcta \t")
