dfb5abcd5bfd012966b188d10ae6fd06fca49808
Pablo Fracaro <70729831+Pablex05@users.noreply.github.com>
2022-05-09 17:04:42 -0300

    Update ipv4_address.py

--- tp1/ipv4_address.py
+++ tp1/ipv4_address.py
@@ -1,16 +1,13 @@
 #Dirección IPv4 (000.000.000.000-255.255.255.255)
 import re
+from pyparsing import Regex
 
-regex = re.compile("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$")
+Regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 
-
-f = open("ipv4.txt","r")
-string = f.readline()
-for string in f:
-    print("\n",string)
-    if regex.search(string):
-        print("Valid address\n")
-    else:
-        print("Invalid address\n")
-
-f.close()
+with open("ipv4.txt") as archivo:
+    for linea in archivo:
+        if re.fullmatch(Regex,linea.strip()):
+            print(f"url: {linea.strip()}\n \t Expresion Correcta \t")
+        else:
+            print(f"url: {linea.strip()}\n \t Expresion Incorrecta \t")
+        print("-------------------------------------------")
