a37e94917f55cd410cb5b075008780efd0f487e5
Pablo Fracaro <70729831+Pablex05@users.noreply.github.com>
2022-05-09 21:01:20 -0300

    Update url.py

--- tp1/url.py
+++ tp1/url.py
@@ -1,6 +1,6 @@
 import re
 
-Regex = "((http|https|HTTP|HTTPS)?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,3})(\/)?"
+Regex = "((http|https|HTTP|HTTPS):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,3})(\/)?"
 
 with open("./url.txt") as archivo:
     for linea in archivo:
